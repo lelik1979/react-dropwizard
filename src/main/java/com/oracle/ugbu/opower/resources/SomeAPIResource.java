dd84e9562ae62229dd121c6bf5ba568f694d5b10
Alexey Dranchuk <alexey.dranchuk@opower.com>
2020-04-02 21:25:10 +0300

    changed API and configuration

--- src/main/java/com/oracle/ugbu/opower/resources/SomeAPIResource.java
+++ src/main/java/com/oracle/ugbu/opower/resources/SomeAPIResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/someApi")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -12,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 public class SomeAPIResource {
 
     @GET
-    public String getById() {
-        return "Some response";
+    public Response getById() {
+        return Response.ok("OK").build();
     }
 }
